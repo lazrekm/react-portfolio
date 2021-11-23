58ef4de5a2e64c58059d464173bf82f7883f0828
Mohamed Lazrek <mohamed.lazrekb@gmail.com>
2021-11-23 10:50:04 -0500

    projects and timeline

---
+++ src/constants/constants.js
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+    title: 'Unearthly Venues',
+    description: "A fully featured web app that observes the MVC (model–view–controller) design pattern. Using the conceit of haunted locations, we provide an intuitive user experience for a user to book a cheap venue for their next party. The application enables users to find events nearby their residence. The website allows the user to schedule an event and book a venue as per their requirements. Having selected their venue, they have now booked their destination for their spooky celebration.",
+      image: '/images/1.png',
+      tags: ['JavaScript', 'Express', 'Handlebars', 'Node'],
+    source: 'https://safe-garden-63694.herokuapp.com/',
+    visit: 'https://github.com/graybishop/the-web-spinners',
+    id: 0,
+  },
+  {
+    title: 'The Daily Felon',
+    description:"Created a satirical newspaper that automatically generated silly news stories about random superheros apprehending criminals, with the heros (Using Marvel API) having a caulous disregard for appropriate use of force, and randomly selected mugshots of the alleged perpetrator (Using JailBase API).",
+    image: '/images/2.png',
+    tags: ['HTML', 'JavaScript', 'CSS'],
+    source: 'https://jamesstitzel.github.io/The-Daily-Felon/',
+    visit: 'https://github.com/JamesStitzel/The-Daily-Felon',
+    id: 1,
+  },
+  {
+    title: 'Weather Dashboard',
+    description: "Created a dashboard that uses the OpenWeather API to retrieve weather data for cities that are searched for by user. Uses localStorage to store any cities that have been searched, and allows user to call them up quickly.",
+      image: '/images/3.jpg',
+      tags: ['HTML', 'CSS', 'JavaScript'],
+    source: 'https://lazrekm.github.io/weather-dashboard/',
+    visit: 'https://github.com/lazrekm/weather-dashboard',
+    id: 2,
+  },
+  {
+    title: 'Website Refactor',
+    description: "This project is a code refactor for the Horiseon webpage. The purposes of this refactoring is to optimize SEO, and make make the previous code more comprehensible and accessible.",
+    image: '/images/4.jpg',
+    tags: ['HTML', 'CSS'],
+    source: 'https://lazrekm.github.io/website-refactoring/',
+    visit: 'https://github.com/lazrekm/website-refactoring',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2021, text: 'Started my journey', },
+];
